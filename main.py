9142a5fefc523e7a28186bc30c47744d028dc1d2
Krelock <Krelockk@yahoo.com>
2024-08-29 14:34:19 -0400

    made  character move

--- main.py
+++ main.py
@@ -27,11 +27,13 @@ def main():
 
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
+        
         pygame.display.flip()
         
         
         
-        dt == clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
